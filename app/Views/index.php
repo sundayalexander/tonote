ec5704a7d7541839b4a81245305dd2a979a57a0e
Sunday Alexander <amowesunday@gmail.com>
2021-07-06 17:02:43 +0100

    Update

--- app/Views/index.php
+++ app/Views/index.php
@@ -131,10 +131,9 @@
     <div class="container">
         <div class="row">
             <!--start about content-->
-            <div class="col-md-6">
-                <div class="about-cont">
-                    <h5>About Arribo</h5>
-                    <h2>Delivering exceptional user experiences.</h2>
+            <div class="col-md-6 pr-lg-5">
+                <div class="about-cont mt-0">
+                    <h2>Become An E-Notary Public</h2>
                     <p>Arribo is designed for those who love to user interface. You will love the seamless way we display the user inter face on your devices.As businesses rely on software or app to engage customers, innovation and velocity becomes core to delivering value.</p>
                     <p>But you’re held back by antiquated technology and legacy development processes. Our customers use our app, to adopt next-generation development practices, deliver new applications, and modernize existing applications.</p>
                 </div>
@@ -164,11 +163,74 @@
                 </div>
             </div>
             <!--end about content-->
+
+            <div class="col-md-6 bg-white pt-4">
+<!--                <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">-->
+                <style type="text/css">
+                    /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                    .contact-form form input {
+                        height: 55px;
+                        border: 0;
+                        border-bottom: 1px solid #e9e9ea;
+                        border-radius: 0;
+                        font-family: 'Open Sans', sans-serif;
+                        font-size: 14px;
+                        background-color: transparent
+                    }
+                    .contact-form form .button{
+                        cursor: pointer;
+                    }
+                    #mc_embed_signup div.mce_inline_error{
+                        color: #ff0000;
+                        margin: 0 0 1em 0;
+                        padding: 5px 10px;
+                        background-color: transparent;
+                        font-weight: normal;
+                        z-index: 1;
+                    }
+
+                </style>
+                <h3 class="text-center mt-4 mb-4">Become A Remote Online Notary</h3>
+                <div id="mc_embed_signup" class="contact-form two">
+
+                    <form action="https://gmail.us6.list-manage.com/subscribe/post?u=51935ac35559b859a69d757cf&amp;id=c8830e3ab5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+                            <div class="mc-field-group form-group">
+                                <input type="email" placeholder="Email *" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+                            </div>
+                            <div class="mc-field-group form-group">
+                                <input type="text" value="" name="FNAME" class="required form-control" id="mce-FNAME" placeholder="First Name*" required>
+                            </div>
+                            <div class="mc-field-group form-group">
+<!--                                <label for="mce-LNAME">Last Name </label>-->
+                                <input type="text" value="" name="LNAME" class="required form-control" id="mce-LNAME" placeholder="Last Name*" required>
+                            </div>
+                            <label for="mce-MMERGE6">Do you have a Traditional Notary Certificate? </label>
+                            <div class="mc-field-group form-group">
+                                <select name="MMERGE6" class="required form-control" id="mce-MMERGE6">
+<!--                                    <option value=""></option>-->
+                                    <option value="YES">YES</option>
+                                    <option value="NO">NO</option>
+
+                                </select>
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_51935ac35559b859a69d757cf_c8830e3ab5" tabindex="-1" value=""></div>
+
+                            <div class="clear">
+                                <input type="submit" value="Submit Now" name="subscribe" id="mc-embedded-subscribe"
+                                       class="button"></div>
+                        </div>
+                    </form>
+                </div>
+                <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[6]='MMERGE6';ftypes[6]='dropdown';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                <!--End mc_embed_signup-->
+            </div>
         </div>
-    </div>
-    <!--start app mocup-->
-    <div class="about-app-mocup">
-        <img src="assets/images/app-mocup-2.png" class="img-fluid" alt="">
     </div>
     <!--end app mocup-->
 </section>
@@ -503,21 +565,11 @@
 <!--start footer-->
 <footer id="footer" class="bg-2">
     <div class="container">
-        <div class="get-started two">
-            <div class="row">
-                <div class="col-lg-6 col-md-8">
-                    <h2>Ready To Get Started?</h2>
-                    <p class="text-light">Don't waste another minute.Create an account now and start saving more time.Do even more with the Arribo.</p>
-                </div>
-                <div class="col-lg-6 col-md-4">
-                    <a href="">Create an Account</a>
-                </div>
-            </div>
-        </div>
-        <div class="footer-cont">
+
+        <div class="footer-cont mb-5">
             <div class="row" >
                 <!--start footer widget single-->
-                <div class="col-lg-4 col-md-6 mx-auto mt-5">
+                <div class="col-lg-4 col-md-6 mx-auto">
                     <div class="footer-widget">
                         <h3>TONote</h3>
                         <ul>
